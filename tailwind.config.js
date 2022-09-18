b438fb0fc0b8ab534e8981034d5292e62ee7598a
Tobilobawebdev <omotadetobiloba@gmail.com>
2022-09-18 02:47:41 +0000

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "Clash": ["Clash Display"],
+        "panchanglight": ["panchang1"],
+      }
+    },
   },
   plugins: [],
 }
